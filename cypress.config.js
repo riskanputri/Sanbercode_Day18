e93d18170f3a2e498aa0ac51d9cb49154622aca3
Riska Nurhana Putri <116998967+riskanputri@users.noreply.github.com>
2023-01-19 12:32:27 +0700

    Add files via upload

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  projectId: 'x7tspq',
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
